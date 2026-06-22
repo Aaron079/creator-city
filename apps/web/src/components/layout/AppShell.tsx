845c055e6c434426e091ee3a910fd9aa0bc88034
Aaron079 <384119917@qq.com>
2026-06-23 02:39:12 +0800

    remove context banner from app shell

--- apps/web/src/components/layout/AppShell.tsx
+++ apps/web/src/components/layout/AppShell.tsx
@@ -1,12 +1,10 @@
 'use client'
 
-import { CurrentContextBar } from '@/components/layout/CurrentContextBar'
 import { TopNavigation } from '@/components/layout/TopNavigation'
 import styles from './nonCanvasShell.module.css'
 
 export function AppShell({
   children,
-  showContextBar = true,
   maxWidth = 'max-w-7xl',
 }: {
   children: React.ReactNode
@@ -16,8 +14,7 @@ export function AppShell({
   return (
     <div className={styles.shell}>
       <TopNavigation />
-      {showContextBar ? <CurrentContextBar /> : null}
-      <main className={`${styles.content} mx-auto ${maxWidth} px-6 ${showContextBar ? 'pt-32' : 'pt-20'} pb-10`}>
+      <main className={`${styles.content} mx-auto ${maxWidth} px-6 pt-20 pb-10`}>
         {children}
       </main>
     </div>
